4ae53be3ef5bf50546a70a0bb3d63ff1571eefb0
ySttick <103322833+ySttick@users.noreply.github.com>
2024-08-17 20:01:30 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,30 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+const displayReto = document.querySelector('.display-reto');
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');    
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');    
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');    
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');    
+});
+
+btnPopup.addEventListener('click', ()=> {
+    displayReto.classList.add('active-popup');    
+});
+
+iconClose.addEventListener('click', ()=> {
+    displayReto.classList.remove('active-popup');    
+});
